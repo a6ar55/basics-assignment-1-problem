634495a08de4d4118a0841424f22318afa78c37f
Adars T S <117556787+a6ar55@users.noreply.github.com>
2023-11-13 09:16:30 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+  const app = Vue.createApp({ 
+    data() { 
+      return { 
+        name: "Adars T S",
+        age: 20,
+        randNum: Math.random() ,
+        myUrl:"https://cdn.thecollector.com/wp-content/uploads/2023/05/ragnar-lodbrok.jpg?width=1400&quality=70",
+
+
+      } 
+    } 
+  }) 
+
+ app.mount('#assignment') 
+
